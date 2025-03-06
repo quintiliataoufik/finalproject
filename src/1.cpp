cf21aded43ccb99e93f38c60fb994b31ef5af746
quintiliataoufik <quintiliataoufik@gmail.com>
2025-03-06 09:29:15 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    int y = 10;
+    if (x > y) {
+        std::cout << "X is greater than Y" << std::endl;
+    } else {
+        std::cout << "Y is greater than X" << std::endl;
+    }
+}
