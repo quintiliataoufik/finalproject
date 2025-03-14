f9667f020eccb2dcdb3847026276ae5eb2bed450
quintiliataoufik <quintiliataoufik@gmail.com>
2025-03-14 19:02:22 +0000

    Add 9.cpp

---
+++ src/9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::string name;
+  int age;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Enter your age: ";
+  std::cin >> age;
+
+  if (age < 18) {
+    std::cout << "Sorry, you are not eligible to vote." << std::endl;
+  } else {
+    std::cout << "Congratulations, you are eligible to vote!" << std::endl;
+  }
+
+  return 0;
+}
