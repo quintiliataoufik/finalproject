9b0a04c34e8faf0b78bb75113dff6e897620295f
quintiliataoufik <quintiliataoufik@gmail.com>
2025-03-25 03:08:36 +0000

    Add 20.cpp

---
+++ src/20.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    if (x > 5) {
+        cout << "Condition is true";
+    } else {
+        cout << "Condition is false";
+    }
+    return 0;
+}
