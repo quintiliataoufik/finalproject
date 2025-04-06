25effc498cddbd93bddc7e1a3169cf56d5275913
quintiliataoufik <quintiliataoufik@gmail.com>
2025-04-06 09:12:26 +0000

    Add 28.cpp

---
+++ src/28.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    int result = num1 * num2; // Multiply two numbers
+    cout << "The product of " << num1 << " and " << num2 << " is: " << result << endl;
+    return 0;
+}
