5dc81f684816181cf179af162ffe2659f8d69534
quintiliataoufik <quintiliataoufik@gmail.com>
2025-05-18 16:07:35 +0000

    Add 40.cpp

---
+++ src/40.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        cout << "Positive number";
+    } else {
+        cout << "Negative number";
+    }
+    return 0;
+}
