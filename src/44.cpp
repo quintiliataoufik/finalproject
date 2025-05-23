4ea6d3127f342059853f70b97da39cdbe082c0f5
quintiliataoufik <quintiliataoufik@gmail.com>
2025-05-23 10:25:11 +0000

    Add 44.cpp

---
+++ src/44.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    if(x > 0) {
+        cout << "x is greater than 0" << endl;
+    } else {
+        cout << "x is not greater than 0" << endl;
+    }
+    return 0;
+}
